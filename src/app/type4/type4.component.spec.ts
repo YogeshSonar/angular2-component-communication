3218b161a312059e8c846e20ed09ebce20c82b44
Yash Kapila <yash.kapila18@gmail.com>
2016-12-08 13:45:45 +1100

    Add files via upload

---
+++ src/app/type4/type4.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { Type4Component } from './type4.component';
+
+describe('Type4Component', () => {
+  let component: Type4Component;
+  let fixture: ComponentFixture<Type4Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Type4Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Type4Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/type4/type4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-type4',
+  templateUrl: './type4.component.html',
+  styleUrls: ['./type4.component.css']
+})
+export class Type4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
